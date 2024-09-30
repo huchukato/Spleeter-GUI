2d81e9bd288dcc2905792795d7f19867b99faa87
GreggaryPeccary <85295616+GreggaryPeccary@users.noreply.github.com>
2024-09-30 08:14:10 +0200

    Update gui.py
    - style refactoring (do not exceed column 80)
    - choice options as a main dictionary

--- gui.py
+++ gui.py
@@ -1,37 +1,43 @@
 #!/usr/bin/env python
 import wx
 import os
-from spleeter.separator import Separator
 import threading
+
+
+SPLEETER_OPTIONS = {
+    "Vocals / accompaniment separation (2 stems)": "2stems",
+    "Vocals / drums / bass / other separation (4 stems)": "4stems",
+    "Vocals / drums / bass / piano / other separation (5 stems)": "5stems",
+}
+
 
 class MyFrame(wx.Frame):
     def __init__(self, parent, title):
         super(MyFrame, self).__init__(parent, title=title, size=(400, 550))
 
         self.panel = wx.Panel(self)
-        
+
         self.image = wx.Image('logo.png', wx.BITMAP_TYPE_PNG)
         self.bitmap = wx.StaticBitmap(self.panel, -1, wx.Bitmap(self.image))
 
         self.file_label = wx.StaticText(self.panel, label="Select File Path:")
         self.folder_label = wx.StaticText(self.panel, label="Select Output Folder:")
-        
+
         self.file_text = wx.TextCtrl(self.panel, style=wx.TE_READONLY)
         self.folder_text = wx.TextCtrl(self.panel, style=wx.TE_READONLY)
-        
+
         self.file_button = wx.Button(self.panel, label="Browse")
         self.Bind(wx.EVT_BUTTON, self.on_select_file, self.file_button)
-        
+
         self.folder_button = wx.Button(self.panel, label="Browse")
         self.Bind(wx.EVT_BUTTON, self.on_select_folder, self.folder_button)
-        
-        options = ["Vocals / accompaniment separation (2 stems)", "Vocals / drums / bass / other separation (4 stems)", "Vocals / drums / bass / piano / other separation (5 stems)"]
+
         self.option_label = wx.StaticText(self.panel, label="Choose the separation mode:")
-        self.option_choice = wx.Choice(self.panel, choices=options)
-        
+        self.option_choice = wx.Choice(self.panel, choices=SPLEETER_OPTIONS.keys())
+
         self.split_button = wx.Button(self.panel, label="Split")
         self.Bind(wx.EVT_BUTTON, self.on_split, self.split_button)
-        
+
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.sizer.Add(self.bitmap, 0, wx.ALIGN_CENTER, 10)
         self.sizer.Add(self.file_label, 0, wx.ALL, 10)
@@ -43,12 +49,13 @@ class MyFrame(wx.Frame):
         self.sizer.Add(self.option_label, 0, wx.ALL, 10)
         self.sizer.Add(self.option_choice, 0, wx.ALL, 10)
         self.sizer.Add(self.split_button, 0, wx.ALL | wx.ALIGN_RIGHT, 10)
-        
+
         self.panel.SetSizer(self.sizer)
         self.Centre()
 
     def on_select_file(self, event):
-        file_dialog = wx.FileDialog(self, "Select a file", wildcard="Audio files (*.mp3;*.wav)|*.mp3;*.wav", style=wx.FD_OPEN)
+        file_dialog = wx.FileDialog(self, "Select a file", wildcard="Audio files (*.mp3;*.wav)|*.mp3;*.wav",
+                                    style=wx.FD_OPEN)
         if file_dialog.ShowModal() == wx.ID_OK:
             file_path = file_dialog.GetPath()
             self.file_text.SetValue(file_path)
@@ -66,18 +73,20 @@ class MyFrame(wx.Frame):
         folder_path = self.folder_text.GetValue()
         option = self.option_choice.GetStringSelection()
         if file_path and folder_path and option:
-            if option == 'Vocals / accompaniment separation (2 stems)': option_spleeter = '2stems'   
-            elif option == 'Vocals / drums / bass / other separation (4 stems)': option_spleeter = '4stems'
-            else: option_spleeter = '5stems'
+            option_spleeter = SPLEETER_OPTIONS[option]
             threading.Thread(target=self.split_audio, args=(file_path, folder_path, option_spleeter)).start()
 
     def split_audio(self, file_path, folder_path, option_spleeter):
         try:
+            from spleeter.separator import Separator
             separator = Separator(f'spleeter:{option_spleeter}')
             separator.separate_to_file(file_path, folder_path)
-            wx.CallAfter(wx.MessageBox, f"Split '{os.path.basename(file_path)}' into '{option_spleeter}' in folder '{folder_path}'", "Operation Completed", wx.OK | wx.ICON_INFORMATION)
+            wx.CallAfter(wx.MessageBox,
+                         f"Split '{os.path.basename(file_path)}' into '{option_spleeter}' in folder '{folder_path}'",
+                         "Operation Completed", wx.OK | wx.ICON_INFORMATION)
         except Exception as e:
             wx.CallAfter(wx.MessageBox, f"Error during separation: {e}", "Error", wx.OK | wx.ICON_ERROR)
+
 
 if __name__ == "__main__":
     app = wx.App(False)
